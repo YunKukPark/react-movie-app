189829806e106fd65ecd7bbc9cf7d4d9ace8b228
YunKukPark <a01043340999@gmail.com>
2021-06-15 16:45:55 +0900

    ⚰️ Delete test code

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import Potato from './Potato';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello !!!!</h1>
-      <Potato />
     </div>
   );
 }
