f9768fde8e72de9e011bfc825d9cdfb0425e5d9a
YunKukPark <a01043340999@gmail.com>
2021-06-24 16:22:36 +0900

    ✨ Add Global navigation

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -5,10 +5,12 @@ import './styles/reset.css';
 import './styles/fonts.css';
 import Home from './routes/Home';
 import About from './routes/About';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <HashRouter>
+      <Navigation />
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
     </HashRouter>
